fdc3ee25a364bf2c163c3c8dc6edf0ee0d7d66b7
ruhul <ruhulamin15jun1999@gmail.com>
2025-02-16 21:28:15 +0600

    shop create is done

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,10 +1,4 @@
-"use client";
-
-import { useUser } from "@/context/UserContext";
-
 const HomePage = () => {
-  const user = useUser();
-  console.log(user);
   return (
     <div>
       <h1>Welcome To NextMart Home Page</h1>
