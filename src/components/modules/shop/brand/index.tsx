31c3c86f82522aa146af7adb79999f72d739b983
ruhul <ruhulamin15jun1999@gmail.com>
2025-02-20 14:35:55 +0600

    Product add, update, delete is done

--- src/components/modules/shop/brand/index.tsx
+++ src/components/modules/shop/brand/index.tsx
@@ -1,25 +1,21 @@
 "use client";
-import { ICategory } from "@/types";
-import CreateCategoryModal from "./CreateCategoryModal";
-import { NMTable } from "@/components/ui/core/NMTable";
+import { NMTable } from "@/components/ui/core/NMTable/index";
 import { ColumnDef } from "@tanstack/react-table";
-import Image from "next/image";
 import { Trash } from "lucide-react";
+import Image from "next/image";
 import { useState } from "react";
-import DeleteConfirmationModal from "@/components/ui/core/NMModal/DeleteConfirmationModal";
+import CreateBrandModal from "./CreateBrandModal";
 import { toast } from "sonner";
-import { deleteCategory } from "@/services/Category";
+import { IBrand } from "@/types";
+import { deleteBrand } from "@/services/Brand";
+import DeleteConfirmationModal from "@/components/ui/core/NMModal/DeleteConfirmationModal";
 
-type TCategoriesProps = {
-  categories: ICategory[];
-};
-
-const ManageCategories = ({ categories }: TCategoriesProps) => {
+const ManageBrands = ({ brands }: { brands: IBrand[] }) => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedId, setSelectedId] = useState<string | null>(null);
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
 
-  const handleDelete = (data: ICategory) => {
+  const handleDelete = (data: IBrand) => {
     console.log(data);
     setSelectedId(data?._id);
     setSelectedItem(data?.name);
@@ -29,7 +25,7 @@ const ManageCategories = ({ categories }: TCategoriesProps) => {
   const handleDeleteConfirm = async () => {
     try {
       if (selectedId) {
-        const res = await deleteCategory(selectedId);
+        const res = await deleteBrand(selectedId);
         console.log(res);
         if (res.success) {
           toast.success(res.message);
@@ -43,14 +39,14 @@ const ManageCategories = ({ categories }: TCategoriesProps) => {
     }
   };
 
-  const columns: ColumnDef<ICategory>[] = [
+  const columns: ColumnDef<IBrand>[] = [
     {
       accessorKey: "name",
-      header: () => <div>Category Name</div>,
+      header: () => <div>Brand Name</div>,
       cell: ({ row }) => (
         <div className="flex items-center space-x-3">
           <Image
-            src={row.original.icon}
+            src={row.original.logo}
             alt={row.original.name}
             width={40}
             height={40}
@@ -95,10 +91,12 @@ const ManageCategories = ({ categories }: TCategoriesProps) => {
   return (
     <div>
       <div className="flex items-center justify-between">
-        <h1 className="text-xl font-bold">Manage Categories</h1>
-        <CreateCategoryModal />
+        <h1 className="text-xl font-bold">Manage Brands</h1>
+
+        <CreateBrandModal />
       </div>
-      <NMTable data={categories} columns={columns} />
+      <NMTable columns={columns} data={brands || []} />
+
       <DeleteConfirmationModal
         name={selectedItem}
         isOpen={isModalOpen}
@@ -109,4 +107,4 @@ const ManageCategories = ({ categories }: TCategoriesProps) => {
   );
 };
 
-export default ManageCategories;
+export default ManageBrands;
